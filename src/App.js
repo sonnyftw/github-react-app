9105912273d58672ec9ad6ec7bfbfa2c1f52ed39
Sunney Qazi <sunneyqazi@gmail.com>
2019-03-19 17:57:56 -0400

    Added Initial Files
    Added the necessary files to create app.

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Header from './components/layout/Header'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+// import uuid from 'uuid';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos : [
+      {
+        id: 1, //uuidv.4(),
+        title : 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2, //uuidv.4(),
+        title : 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3, //uuidv.4(),
+        title : 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+// Toggle Complete  
+markComplete = (id) => {
+  this.setState({todos: this.state.todos.map(todo => {
+    if (todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo;
+  }) });
+}
+
+// Delete Todo
+delTodo = (id) => {
+  this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+}
+
+// Add Todo
+addTodo = (title) => {
+  const newTodo = {
+    id: 4, //uuidv.4(),
+    title,
+    completed: false
+  }
+  this.setState({todos: [...this.state.todos, newTodo]})
+} 
+
+  render() {
+    return (
+      <div className="App">
+      <div className="container">
+        <Header/>
+        <AddTodo addTodo={this.addTodo} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
